1760f5989fb060627c5b6fcab8f29505d5dfa867
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-25 21:43:03 +0700

    fixing binary

--- I0320017_soal 6_tugas 4.py
+++ I0320017_soal 6_tugas 4.py
@@ -1,6 +1,38 @@
-print("4 | 11:", 4 | 11)
-print("4 >> 11:", 4 >> 11)
-print("4 ^ 11:", 4 ^ 11)
-print("~4:", ~4)
-print("11 & 4:", 11 & 4)
+a = 4
+b = 11
+c = a | b
+print("---------- OR -----------")
+print('binary dari', a, 'adalah', format(a, '08b'))
+print('binary dari', b, 'adalah', format(b, '08b'))
+print('binary dari', c, 'adalah', format(c, '08b'))
+print("------------------------")
+
+c = a >> b
+print("---------- RIGHT SHIFT -----------")
+print('binary dari', a, 'adalah', format(a, '08b'))
+print('binary dari', b, 'adalah', format(b, '08b'))
+print('binary dari', c, 'adalah', format(c, '08b'))
+print("----------------------------------")
+
+c = a ^ b
+print("---------- XOR -----------")
+print('binary dari', a, 'adalah', format(a, '08b'))
+print('binary dari', b, 'adalah', format(b, '08b'))
+print('binary dari', c, 'adalah', format(c, '08b'))
+print("--------------------------")
+
+c = ~a
+print("---------- NOT -----------")
+print('binary dari', a, 'adalah', format(a, '08b'))
+print('binary dari', b, 'adalah', format(b, '08b'))
+print('binary dari', c, 'adalah', format(c, '08b'))
+print("--------------------------")
+
+c = a & b
+print("---------- AND -----------")
+print('binary dari', a, 'adalah', format(a, '08b'))
+print('binary dari', b, 'adalah', format(b, '08b'))
+print('binary dari', c, 'adalah', format(c, '08b'))
+print("--------------------------")
+
 
